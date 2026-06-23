5efc1bdbd4cb5b95e584d2f195dcd1d9424fe75d
Youssef Ahmed <youssefahemd729@gmail.com>
2026-06-23 21:59:51 +0300

    change the investor page 1

--- src/Pages/InvestorPage/InvestorPage.jsx
+++ src/Pages/InvestorPage/InvestorPage.jsx
@@ -5,36 +5,35 @@ import axios from 'axios'
 import SecondNavbar from '../../Components/Common/SecondNavbar'
 
 export default function Investors() {
-  const [investors, setInvestors] = useState([])
+    const [investors, setInvestors] = useState([])
 
-  useEffect(() => {
-    fetchInvestors()
-  }, [])
+    useEffect(() => {
+        fetchInvestors()
+    }, [])
 
-  const fetchInvestors = async () => {
-    try {
-      const res = await axios.get('/api/users/investors')
-      setInvestors(res.data.investors)
-    } catch (err) {
-      console.log(err)
+    const fetchInvestors = async () => {
+        try {
+            const res = await axios.get('/api/users/investors')
+            setInvestors(res.data.investors)
+        } catch (err) {
+            console.log(err)
+        }
     }
-  }
 
-  return (
-      <>
-    <SecondNavbar />
-    <Container className="py-4">
-      
+    return (
+        <>
+            <SecondNavbar />
+            <Container className="py-4">
 
-      <Row>
-        {investors.map((investor) => (
-          <Col md={4} key={investor._id} className="mb-4">
-            <InvestorCard investor={investor} />
-          </Col>
-        ))}
-      </Row>
-    </Container>
-  
- </>
-)
+                    <Row>
+                        {investors.map((investor) => (
+                            <Col md={4} key={investor._id} className="mb-4">
+                                <InvestorCard investor={investor} />
+                            </Col>
+                        ))}
+                    </Row>
+            </Container>
+
+        </>
+    )
 }
